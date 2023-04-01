8a6c5a602cf79c3ba869c6378ca11c388383abb1
Jonathan Guyer <guyer@nist.gov>
2023-03-31 23:15:27 -0400

    Dockerize

--- ntd2d_action/action.py
+++ ntd2d_action/action.py
@@ -1,4 +1,3 @@
-import argparse
 import git
 import os
 from packaging.version import parse, InvalidVersion
@@ -248,27 +247,3 @@ class NISTtheDocs2Death(object):
         self.write_index_html()
 
         self.commit()
-
-
-def main():
-    description = 'Update nist-pages branch based on sphinx builds'
-    parser = argparse.ArgumentParser(
-                        prog='NistTheDocs2Death',
-                        description=description)
-
-    parser.add_argument('docs_dir')
-    parser.add_argument('default_branch')
-    parser.add_argument('pages_branch')
-    parser.add_argument('pages_url')
-
-    args = parser.parse_args()
-
-    ntd2d = NISTtheDocs2Death(docs_dir=args.docs_dir,
-                              default_branch=args.default_branch,
-                              pages_branch=args.pages_branch,
-                              pages_url=args.pages_url)
-    ntd2d.update_pages()
-
-
-if __name__ == "__main__":
-    main()
