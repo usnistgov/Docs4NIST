f3e23a73ce75343d45275ce9e408758c0fbf7d3b
Jonathan Guyer <guyer@nist.gov>
2023-04-01 09:22:53 -0400

    Change action argument to an input

--- update_pages/entrypoint.py
+++ update_pages/entrypoint.py
@@ -5,22 +5,15 @@ import os
 from ntd2d_action.action import NISTtheDocs2Death
 
 def main():
-    description = 'Update nist-pages branch based on sphinx builds'
-    parser = argparse.ArgumentParser(
-                        prog='NISTtheDocs2Death',
-                        description=description)
+    action = os.environ['INPUT_ACTION']
 
-    parser.add_argument('action', choices=['borg_the_docs', 'update_pages'])
-
-    args = parser.parse_args()
-
-    if args.action == 'update_pages':
+    if action == 'update_pages':
         ntd2d = NISTtheDocs2Death(docs_dir=os.environ['INPUT_DOCS-FOLDER'],
                                   default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
                                   pages_branch=os.environ['INPUT_PAGES-BRANCH'],
                                   pages_url=os.environ['INPUT_PAGES-URL'])
         ntd2d.update_pages()
-    elif args.action = 'borg_the_docs':
+    elif action = 'borg_the_docs':
         print("Borging the docs")
 
 if __name__ == "__main__":
