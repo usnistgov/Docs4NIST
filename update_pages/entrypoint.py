13659fc5db37da128ccc86b6cc745827c908ffce
Jonathan Guyer <guyer@nist.gov>
2023-04-10 22:57:03 -0400

    Update entrypoint for refactored classes

--- update_pages/entrypoint.py
+++ update_pages/entrypoint.py
@@ -2,17 +2,24 @@
 import argparse
 import os
 
-from ntd2d_action.action import NISTtheDocs2Death
+from ntd2d_action.repository import Repository
+from ntd2d_action.sphinxdocs import SphinxDocs
 
 def main():
     action = os.environ['INPUT_ACTION']
 
     if action == 'update_pages':
-        ntd2d = NISTtheDocs2Death(docs_dir=os.environ['INPUT_DOCS-FOLDER'],
-                                  default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
-                                  pages_branch=os.environ['INPUT_PAGES-BRANCH'],
-                                  pages_url=os.environ['INPUT_PAGES-URL'])
-        ntd2d.update_pages()
+        docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
+
+        repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
+                          repository=os.environ['GITHUB_REPOSITORY'],
+                          branch=os.environ['INPUT_PAGES-BRANCH'],
+                          default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
+                          docs=docs,
+                          pages_url=os.environ['INPUT_PAGES-URL'])
+
+        repo.update_pages(branch=os.environ['GITHUB_REF_NAME'],
+                          sha=os.environ['GITHUB_SHA'])
     elif action == 'borg_the_docs':
         print("Borging the docs")
 
