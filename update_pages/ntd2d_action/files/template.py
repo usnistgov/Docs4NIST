d9bb3d85ee8f56d164b99503260dcafa5af13daf
Jonathan Guyer <guyer@nist.gov>
2023-04-14 16:07:15 -0400

    Introduce TemplateHierarchy

--- update_pages/ntd2d_action/files/template.py
+++ update_pages/ntd2d_action/files/template.py
@@ -1,10 +1,8 @@
 import pathlib
 
 class Template:
-    def __init__(self, name):
-        # look in templates/ directory adjacent to this file
-        dir = pathlib.Path(__file__).parent
-        self.template_path = dir / "templates" / name
+    def __init__(self, template_path):
+        self.template_path = template_path
 
     def read(self):
         with open(self.template_path, mode='r') as template_file:
@@ -12,18 +10,18 @@ class Template:
 
         return template
 
-class PagesTemplate(Template):
+class FileTemplate(Template):
+    def __init__(self, name):
+        # look in templates/ directory adjacent to this file
+        self.template_dir = pathlib.Path(__file__).parent / "templates"
+        super().__init__(template_path=self.template_dir / name)
+
+class PagesTemplate(FileTemplate):
     def __init__(self, working_dir, name):
         # look in _templates/ directory of nist-pages branch
         self.template_path = working_dir / "_templates" / name
         if not self.template_path.exists():
             super().__init__(name=name)
-
-    def read(self):
-        with open(self.template_path, mode='r') as template_file:
-            template = template_file.read()
-
-        return template
 
 class IFrame(PagesTemplate):
     def __init__(self, working_dir):
