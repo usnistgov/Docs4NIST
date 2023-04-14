e29aa16ded0afce192840ddc981cb6e8b0f373cd
Jonathan Guyer <guyer@nist.gov>
2023-04-14 16:30:52 -0400

    Add ConfFile

--- borg_the_docs/ntd2d_action/sphinxdocs.py
+++ borg_the_docs/ntd2d_action/sphinxdocs.py
@@ -1,44 +1,25 @@
-import importlib
 import pathlib
-import textwrap
 
-import os
-
-from .files import FileTemplate, TemplateHierarchy
+from .files import ConfFile, TemplateHierarchy
 
 
 class SphinxDocs:
     def __init__(self, docs_dir):
         self.docs_dir = pathlib.Path(docs_dir)
-        self.conf_file = self.docs_dir / "conf.py"
 
     @property
     def html_dir(self):
         return self.docs_dir / "_build" / "html"
 
-    def read_conf(self):
-        # The Sphinx docs says that it
-        # [reads conf.py with `importlib.import_module`](https://www.sphinx-doc.org/en/master/usage/configuration.html#module-conf)
-        # [It doesn't](https://github.com/sphinx-doc/sphinx/blob/2c83af0aab7080e0b78d4a16981eed878b2cac4c/sphinx/config.py#L353).
-        namespace = {}
-        namespace['__file__'] = self.conf_file.as_posix()
-
-        with self.conf_file.open(mode='rb') as f:
-            code = compile(f.read(), self.conf_file, 'exec')
-            exec(code, namespace)  # NoQA: S102
-
-        return namespace
-
     def inject_theme(self):
-        conf = self.read_conf()
-        inherited_theme = conf.get("html_theme", "default")
+        conf = ConfFile(docs_dir=self.docs_dir)
+        configuration = conf.read()
+
+        inherited_theme = configuration.get("html_theme", "default")
 
         templates = TemplateHierarchy(name="ntd2d",
                                       destination_dir=self.docs_dir,
                                       inherited_theme=inherited_theme)
         templates.write()
 
-        suffix = FileTemplate(name="conf_suffix.py")
-
-        with self.conf_file.open(mode='a') as f:
-            f.write(suffix.read())
+        conf.write()
