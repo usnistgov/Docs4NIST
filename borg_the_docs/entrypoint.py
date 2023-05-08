892d9f95ec312311ed91c78bfe9cfdf99ee975d4
Jonathan Guyer <guyer@nist.gov>
2023-05-08 09:49:23 -0400

    Cleanup

--- borg_the_docs/entrypoint.py
+++ borg_the_docs/entrypoint.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-import argparse
 import os
 
 from ntd2d_action.repository import Repository
 from ntd2d_action.sphinxdocs import SphinxDocs
 
+
 def main():
     action = os.environ['INPUT_ACTION']
+    docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
 
     if action == 'update_pages':
-        docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
-
         repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
                           repository=os.environ['GITHUB_REPOSITORY'],
                           branch=os.environ['INPUT_PAGES-BRANCH'],
@@ -21,11 +20,7 @@ def main():
         repo.update_pages(branch=os.environ['GITHUB_REF_NAME'],
                           sha=os.environ['GITHUB_SHA'])
     elif action == 'borg_the_docs':
-        print("Borging the docs")
-        docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
-        print("Borging...")
         docs.assimilate_theme()
-        print("...the docs")
 
 if __name__ == "__main__":
     main()
