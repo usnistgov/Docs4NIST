8b74a853b4caaf5bf4bbbe3383ac164c3e8ac045
Jonathan Guyer <guyer@nist.gov>
2023-05-12 23:29:52 -0400

    Refactor file writing

--- update_pages/ntd2d_action/repository.py
+++ update_pages/ntd2d_action/repository.py
@@ -2,7 +2,7 @@ import git
 import github_action_utils as gha_utils
 import pathlib
 
-from .files import NoJekyllFile, VariantsFile, MenuFile, IndexFile
+from .files import NoJekyllFile
 from .variants import Variant, VariantCollection
 
 class Repository:
@@ -45,28 +45,9 @@ class Repository:
         NoJekyllFile(repo=self).write()
 
         # replace any built documents in directory named for current branch
-        variant = Variant(repo=self, name=branch)
-        variant.copy_dir(src=self.docs.html_dir)
+        Variant(repo=self, name=branch).copy_dir(src=self.docs.html_dir)
 
-        if variant.name == self.default_branch:
-            # replace any built documents in latest/
-            # (but only do this for default branch of repo)
-            latest = variant.clone("latest")
-
-        variants = VariantsFile(repo=self,
-                                variants=VariantCollection(repo=self),
-                                pages_url=self.pages_url)
-        variants.write()
-
-        # Need an absolute url because this gets included from
-        # many different levels
-        MenuFile(repo=self,
-                 current_branch=branch,
-                 variants_url=variants.get_url().geturl()).write()
-
-        # This can be a relative url, because all variants should
-        # be on the same server
-        IndexFile(repo=self, variants_url=variants.get_url().path).write()
+        VariantCollection(repo=self).write_files(pages_url=self.pages_url)
 
         self.commit(message=f"Update documentation for {branch}@{sha[:7]}")
 
