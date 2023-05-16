603b59d254c9a6e5577a68e99ad209f6e8c11686
Jonathan Guyer <guyer@nist.gov>
2023-05-16 11:51:58 -0400

    Add diagnostics

--- borg_the_docs/entrypoint.sh
+++ borg_the_docs/entrypoint.sh
@@ -2,9 +2,12 @@
 
 requirements="${INPUT_DOCS-FOLDER}/requirements.txt"
 
+echo "::notice::requirements=${requirements}"
 if [ -f $requirements ]; 
 then
+    echo "::notice::installing"
     pip install -r $requirements
+    echo "::notice::`python -m pip freeze`"
 fi
 
 python /entrypoint.py >> $GITHUB_OUTPUT
