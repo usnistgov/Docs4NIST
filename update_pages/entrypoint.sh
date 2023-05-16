a0c1819014a14715d83f4e9235d914af068de714
Jonathan Guyer <guyer@nist.gov>
2023-05-16 12:17:57 -0400

    More more diagnostics

--- update_pages/entrypoint.sh
+++ update_pages/entrypoint.sh
@@ -2,7 +2,11 @@
 
 echo "::warning::INPUT_ACTION=${INPUT_ACTION}"
 echo "::warning::GITHUB_SHA=${GITHUB_SHA}"
-echo "::warning::GITHUB_SHA=INPUT_DOCS-FOLDER=${INPUT_DOCS-FOLDER}"
+echo "::warning::INPUT_ACTION=${INPUT_ACTION}"
+echo "::warning::INPUT_DOCS-FOLDER=${INPUT_DOCS-FOLDER}"
+echo "::warning::INPUT_DEFAULT-BRANCH=${INPUT_DEFAULT-BRANCH}"
+echo "::warning::INPUT_PAGES-BRANCH=${INPUT_PAGES-BRANCH}"
+echo "::warning::INPUT_PAGES-URL=${INPUT_PAGES-URL}"
 
 requirements="${INPUT_DOCS-FOLDER}/requirements.txt"
 
