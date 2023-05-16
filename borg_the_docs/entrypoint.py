aac66e1ffedefa0dbb25c418f8e7835aba00a696
Jonathan Guyer <guyer@nist.gov>
2023-05-15 22:40:59 -0400

    Install requirements

--- borg_the_docs/entrypoint.py
+++ borg_the_docs/entrypoint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import subprocess
 
 from ntd2d_action.repository import Repository
 from ntd2d_action.sphinxdocs import SphinxDocs
@@ -8,6 +9,12 @@ from ntd2d_action.sphinxdocs import SphinxDocs
 def main():
     action = os.environ['INPUT_ACTION']
     docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
+
+    # Install any requirements for documentation.
+    # Adapted from ammaraskar/sphinx-action
+    requirements = os.path.join(docs, "requirements.txt")
+    if os.path.exists(requirements):
+        subprocess.check_call(["pip", "install", "-r", requirements])
 
     if action == 'update_pages':
         repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
