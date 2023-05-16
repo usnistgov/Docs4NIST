d2933af3da131d81d0bf87ccabaab3637c5a5833
Jonathan Guyer <guyer@nist.gov>
2023-05-16 10:34:22 -0400

    Bootstrap pip installation

---
+++ borg_the_docs/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+requirements="${INPUT_DOCS-FOLDER}/requirements.txt"
+
+if [ -f $requirements ]; 
+then
+    pip install -r $requirements
+fi
+
+python entrypoint.py >> $GITHUB_OUTPUT
