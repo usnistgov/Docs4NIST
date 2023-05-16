aa668668f200b2e45124fa8c9b48a11b2e1304dd
Jonathan Guyer <guyer@nist.gov>
2023-05-16 12:23:40 -0400

    More more more

--- update_pages/entrypoint.sh
+++ update_pages/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+echo "::error::`env`"
 echo "::warning::INPUT_ACTION=${INPUT_ACTION}"
 echo "::warning::GITHUB_SHA=${GITHUB_SHA}"
-echo "::warning::INPUT_ACTION=${INPUT_ACTION}"
 echo "::warning::INPUT_DOCS-FOLDER=${INPUT_DOCS-FOLDER}"
 echo "::warning::INPUT_DEFAULT-BRANCH=${INPUT_DEFAULT-BRANCH}"
 echo "::warning::INPUT_PAGES-BRANCH=${INPUT_PAGES-BRANCH}"
