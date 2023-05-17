a09aab50529a7f8239acd4bb875b72cc8c8cb942
Jonathan Guyer <guyer@nist.gov>
2023-05-17 09:02:48 -0400

    Change hyphens in action inputs to underscores
    The GitHub Action docs
    [illustrate](https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#example-specifying-inputs)
    using hyphens in input names, and then document that names like
    `octocat-eye-color` are converted to environment variables like
    `INPUT_OCTOCAT-EYE-COLOR`, but these environment variable names
    are [not POSIX compliant](https://stackoverflow.com/a/36992531/2019542)
    and can't be accessed in many shells.
    They've [known about this](https://github.com/actions/toolkit/issues/629) for two years.
    Grrrr.

--- update_pages/entrypoint.py
+++ update_pages/entrypoint.py
@@ -8,21 +8,15 @@ from ntd2d_action.sphinxdocs import SphinxDocs
 
 def main():
     action = os.environ['INPUT_ACTION']
-    docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
-
-#     # Install any requirements for documentation.
-#     # Adapted from ammaraskar/sphinx-action
-#     requirements = os.path.join(docs.docs_dir, "requirements.txt")
-#     if os.path.exists(requirements):
-#         subprocess.check_call(["pip", "install", "-r", requirements])
+    docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS_FOLDER'])
 
     if action == 'update_pages':
         repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
                           repository=os.environ['GITHUB_REPOSITORY'],
-                          branch=os.environ['INPUT_PAGES-BRANCH'],
-                          default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
+                          branch=os.environ['INPUT_PAGES_BRANCH'],
+                          default_branch=os.environ['INPUT_DEFAULT_BRANCH'],
                           docs=docs,
-                          pages_url=os.environ['INPUT_PAGES-URL'])
+                          pages_url=os.environ['INPUT_PAGES_URL'])
 
         repo.update_pages(branch=os.environ['SANITIZED_REF_NAME'],
                           sha=os.environ['GITHUB_SHA'])
