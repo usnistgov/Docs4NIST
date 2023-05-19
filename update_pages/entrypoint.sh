4d4558df8073f019125ac5176f1d669aeb195917
Jonathan Guyer <guyer@nist.gov>
2023-05-19 13:43:15 -0400

    Simplify diagnostics

--- update_pages/entrypoint.sh
+++ update_pages/entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-echo "::warning::`env`"
-echo "::warning::INPUT_ACTION=${INPUT_ACTION}"
-echo "::warning::GITHUB_SHA=${GITHUB_SHA}"
-echo "::warning::INPUT_DOCS_FOLDER=${INPUT_DOCS_FOLDER}"
-echo "::warning::INPUT_DEFAULT_BRANCH=${INPUT_DEFAULT_BRANCH}"
-echo "::warning::INPUT_PAGES_BRANCH=${INPUT_PAGES_BRANCH}"
-echo "::warning::INPUT_PAGES_URL=${INPUT_PAGES_URL}"
-
 requirements="${INPUT_DOCS_FOLDER}/requirements.txt"
 
-echo "::warning::requirements=${requirements}"
 if [ -f $requirements ]; 
 then
-    echo "::notice::installing"
     pip install -r $requirements
+    echo "::group::pip"
     echo "::notice::`python -m pip freeze`"
+    echo "::endgroup::"
 fi
 
 python /entrypoint.py >> $GITHUB_OUTPUT
