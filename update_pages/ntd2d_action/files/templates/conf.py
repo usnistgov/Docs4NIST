4da3bc5b40e2f8a138f731b9b948ce08e57cbaf2
Jonathan Guyer <guyer@nist.gov>
2023-05-22 12:06:04 -0400

    Move theme assimilation into ConfFile

---
+++ update_pages/ntd2d_action/files/templates/conf.py
@@ -0,0 +1,6 @@
+{original_contents}
+
+# -- Injected automatically by NISTtheDocs2Death------------------------------
+
+html_theme = {html_theme}
+html_theme_path = {html_theme_path}
--- update_pages/ntd2d_action/sphinxdocs.py
+++ update_pages/ntd2d_action/sphinxdocs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pathlib
 
-from .files import ConfFile, TemplateHierarchy
+from .files import ConfFile
 
 
 class SphinxDocs:
@@ -14,13 +14,5 @@ class SphinxDocs:
 
     def assimilate_theme(self):
         conf = ConfFile(docs_dir=self.docs_dir)
-        configuration = conf.read()
-
-        inherited_theme = configuration.get("html_theme", "default")
-
-        templates = TemplateHierarchy(name="ntd2d",
-                                      destination_dir=self.docs_dir,
-                                      inherited_theme=inherited_theme)
-        templates.write()
-
+        conf.assimilate_theme(name="ntd2d")
         conf.write()
