2b0cfe2b0e84b0ef9bc22ea2b0ed44a47f911e7d
Jonathan Guyer <guyer@nist.gov>
2023-05-24 23:47:56 -0400

    Add diagnostics

--- update_pages/entrypoint.py
+++ update_pages/entrypoint.py
@@ -25,7 +25,7 @@ def main():
     elif action == 'borg_the_docs':
         completed = subprocess.run(["pwd"], capture_output=True, text=True)
         gha_utils.error("borging pwd: " + completed.stdout)
-        completed = subprocess.run(["ls", "-l"], capture_output=True, text=True)
+        completed = subprocess.run(["ls", "-la"], capture_output=True, text=True)
         gha_utils.error("borging ls: " + completed.stdout)
         completed = subprocess.run(["python", "setup.py", "version"], capture_output=True, text=True)
         gha_utils.error("borging version: " + completed.stdout)
