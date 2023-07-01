1c6715e3249778c149d4691b409e85c1fa348314
Jonathan Guyer <guyer@nist.gov>
2023-07-01 09:17:39 -0400

    Stub out documentation directory
    Combined source/_build layout
    https://www.sphinx-doc.org/en/master/tutorial/getting-started.html#creating-the-documentation-layout

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NISTtheDocs2Death'
+copyright = '2023, Jonathan E. Guyer'
+author = 'Jonathan E. Guyer'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
