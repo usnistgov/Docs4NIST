d7bfcbabd074d6efefd9430e92b848e47ec04730
Jonathan Guyer <guyer@nist.gov>
2023-07-11 12:29:23 -0400

    Port ConfFile from borg_the_docs
    Use to get project name

---
+++ update_pages/ntd2d_action/files/conffile.py
@@ -0,0 +1,81 @@
+import contextlib
+import github_action_utils as gha_utils
+import os
+import pathlib
+
+from .file import File
+
+
+# By [Lukas](https://stackoverflow.com/users/911441/lukas)
+# [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
+# https://stackoverflow.com/a/42441759/2019542
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = pathlib.Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+class ConfFile(File):
+    def __init__(self, docs_dir, source_rel=""):
+        self.docs_dir = pathlib.Path(docs_dir)
+        self.source_dir = self.docs_dir / source_rel
+        self.theme = None
+        self._code = None
+        self._configuration = None
+
+    @property
+    def configuration(self):
+        if self._configuration is None:
+            self._configuration = self.read()
+        return self._configuration
+
+    @property
+    def html_theme_path(self):
+        html_theme_path = self.configuration.get("html_theme_path", [])
+
+        relative_path = self.theme_path.relative_to(self.source_dir).as_posix()
+        if relative_path not in html_theme_path:
+            html_theme_path.append(relative_path)
+
+        return html_theme_path
+
+    @property
+    def exclude_patterns(self):
+        return self.configuration.get("exclude_patterns", [])
+
+    @property
+    def path(self):
+        return self.source_dir / "conf.py"
+
+    @property
+    def project(self):
+        return self.configuration.project
+
+    @property
+    def theme_path(self):
+        return self.source_dir / "_themes"
+
+    def read(self):
+        # The Sphinx docs says that it
+        # [reads conf.py with `importlib.import_module`](https://www.sphinx-doc.org/en/master/usage/configuration.html#module-conf)
+        # [It doesn't](https://github.com/sphinx-doc/sphinx/blob/2c83af0aab7080e0b78d4a16981eed878b2cac4c/sphinx/config.py#L353).
+        namespace = {}
+        namespace['__file__'] = self.path.as_posix()
+
+        code = compile(self.original_contents, self.path, 'exec')
+
+        with working_directory(self.source_dir):
+            exec(code, namespace)  # NoQA: S102
+
+        return namespace
+
+    def get_contents(self):
+        if self._code is None:
+            with self.path.open(mode='r') as f:
+                self._code = f.read()
+
+        return self._code
--- update_pages/ntd2d_action/sphinxdocs.py
+++ update_pages/ntd2d_action/sphinxdocs.py
@@ -7,17 +7,19 @@ class SphinxDocs:
         self.docs_dir = pathlib.Path(docs_dir)
         self.build_dir = self.docs_dir / self.build_rel
 
+        self.conf = ConfFile(docs_dir=docs_dir, source_rel=self.source_rel)
+
     @property
     def html_dir(self):
         return self.build_dir / "html"
 
     @property
     def epub_file(self):
-        return self.build_dir / "epub" / ???.epub
+        return self.build_dir / "epub" / f"{self.conf.project}.epub"
 
     @property
     def pdf_file(self):
-        return self.build_dir / "latex" / ???.pdf
+        return self.build_dir / "latex" / f"{self.conf.project.tolower()}.pdf"
 
 class SeparatedSphinxDocs(SphinxDocs):
     build_rel = "build"
