1091e95b5ddb59d4cafa2bfdebe838619157cb9b
Jonathan Guyer <guyer@nist.gov>
2023-07-14 15:17:14 -0500

    Fix singleton initialization

--- update_pages/ntd2d_action/variants.py
+++ update_pages/ntd2d_action/variants.py
@@ -94,13 +94,13 @@ class Version(Variant):
         else:
             return super().__lt__(other)
 
-class VariantCollection:
+class VariantCollection(object):
     # singleton
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(VariantCollection, cls).__new__(cls)
+            cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
 
     def __init__(self, repo, current_variant):
