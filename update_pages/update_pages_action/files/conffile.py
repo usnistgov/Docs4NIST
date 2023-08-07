9fb3462eeeb6744cfa130538162297bc4f929909
Jonathan Guyer <guyer@nist.gov>
2023-08-07 18:57:35 -0400

    DEBUG: see configuration

--- update_pages/update_pages_action/files/conffile.py
+++ update_pages/update_pages_action/files/conffile.py
@@ -34,6 +34,7 @@ class ConfFile(File):
 
         if self._configuration is None:
             self._configuration = self.read()
+            gha_utils.debug(f"configuration: {self._configuration}")
         return self._configuration
 
     @property
