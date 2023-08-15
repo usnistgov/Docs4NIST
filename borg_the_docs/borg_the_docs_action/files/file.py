52430fd081a2627625eaf53e62cc5be5f7e3333d
Jonathan Guyer <guyer@nist.gov>
2023-08-15 13:40:03 -0400

    Turn working_directory into a static method

--- borg_the_docs/borg_the_docs_action/files/file.py
+++ borg_the_docs/borg_the_docs_action/files/file.py
@@ -1,3 +1,5 @@
+import contextlib
+
 class File:
     """Base file class
     """
@@ -10,6 +12,20 @@ class File:
 
     def get_contents(self):
         return ""
+
+    # By [Lukas](https://stackoverflow.com/users/911441/lukas)
+    # [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
+    # https://stackoverflow.com/a/42441759/2019542
+    @contextlib.contextmanager
+    @staticmethod
+    def working_directory(path):
+        """Changes working directory and returns to previous on exit."""
+        prev_cwd = pathlib.Path.cwd()
+        os.chdir(path)
+        try:
+            yield
+        finally:
+            os.chdir(prev_cwd)
 
     def write(self):
         self.path.parent.mkdir(parents=True, exist_ok=True)
--- update_pages/update_pages_action/files/file.py
+++ update_pages/update_pages_action/files/file.py
@@ -1,3 +1,5 @@
+import contextlib
+
 class File:
     def __init__(self):
         pass
@@ -8,6 +10,20 @@ class File:
 
     def get_contents(self):
         return ""
+
+    # By [Lukas](https://stackoverflow.com/users/911441/lukas)
+    # [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
+    # https://stackoverflow.com/a/42441759/2019542
+    @contextlib.contextmanager
+    @staticmethod
+    def working_directory(path):
+        """Changes working directory and returns to previous on exit."""
+        prev_cwd = pathlib.Path.cwd()
+        os.chdir(path)
+        try:
+            yield
+        finally:
+            os.chdir(prev_cwd)
 
     def write(self):
         self.path.parent.mkdir(parents=True, exist_ok=True)
