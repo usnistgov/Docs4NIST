00b71d286212194dffd48adeebe1821a54caca9a
Jonathan Guyer <guyer@nist.gov>
2023-08-24 19:34:31 -0400

    Unify documentation

--- ntd2d/ntd2d.py
+++ ntd2d/ntd2d.py
@@ -12,7 +12,6 @@ def main():
     docs = BorgedSphinxDocs(original_docs=original_docs)
     docs.assimilate_theme(name="ntd2d")
     
-    gha_utils.set_output("borged-docs-folder", docs.docs_dir.as_posix())
     gha_utils.set_output("borged-build-folder", docs.build_dir.as_posix())
 
     repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
