e7d391997e3eaafdb3a1c9897d66d536999c4b94
Jonathan Guyer <guyer@nist.gov>
2023-08-24 22:48:56 -0400

    Annotate GH Action groups

--- ntd2d/ntd2d.py
+++ ntd2d/ntd2d.py
@@ -8,21 +8,23 @@ from ntd2d_action.repository import Repository
 
 
 def main():
-    original_docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
-    docs = BorgedSphinxDocs(original_docs=original_docs)
-    docs.assimilate_theme(name="ntd2d")
+    with gha_utils.group("Borg the Docs"):
+        original_docs = SphinxDocs(docs_dir=os.environ['INPUT_DOCS-FOLDER'])
+        docs = BorgedSphinxDocs(original_docs=original_docs)
+        docs.assimilate_theme(name="ntd2d")
     
-    gha_utils.set_output("borged-build-folder", docs.build_dir.as_posix())
+        gha_utils.set_output("borged-build-folder", docs.build_dir.as_posix())
 
-    repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
-                      repository=os.environ['GITHUB_REPOSITORY'],
-                      branch=os.environ['INPUT_PAGES-BRANCH'],
-                      default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
-                      docs=docs,
-                      pages_url=os.environ['INPUT_PAGES-URL'])
+    with gha_utils.group("Update Pages"):
+        repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
+                          repository=os.environ['GITHUB_REPOSITORY'],
+                          branch=os.environ['INPUT_PAGES-BRANCH'],
+                          default_branch=os.environ['INPUT_DEFAULT-BRANCH'],
+                          docs=docs,
+                          pages_url=os.environ['INPUT_PAGES-URL'])
 
-    repo.update_pages(branch=os.environ['SANITIZED_REF_NAME'],
-                      sha=os.environ['GITHUB_SHA'])
+        repo.update_pages(branch=os.environ['SANITIZED_REF_NAME'],
+                          sha=os.environ['GITHUB_SHA'])
 
 if __name__ == "__main__":
     try:
