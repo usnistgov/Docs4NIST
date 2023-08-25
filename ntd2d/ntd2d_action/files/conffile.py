14c89ac7cf2f95c87068bdd3f79d4cf13945eb0a
Jonathan Guyer <guyer@nist.gov>
2023-08-25 19:09:47 -0400

    DEBUG: Check configuration keys

--- ntd2d/ntd2d_action/files/conffile.py
+++ ntd2d/ntd2d_action/files/conffile.py
@@ -67,6 +67,7 @@ class ConfFile(File):
 
     @property
     def project(self):
+        gha_utils.debug("configuration = {self.configuration.keys()}")
         return self.configuration["project"]
 
     @property
