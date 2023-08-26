6baa0a56192798ef575d05317f3507c1c5e434cc
Jonathan Guyer <guyer@nist.gov>
2023-08-25 20:02:22 -0400

    DEBUG: see code

--- ntd2d/ntd2d_action/files/conffile.py
+++ ntd2d/ntd2d_action/files/conffile.py
@@ -81,6 +81,7 @@ class ConfFile(File):
         namespace = {}
         namespace['__file__'] = self.path.resolve().as_posix()
 
+        gha_utils.debug("code = {self.original_contents}")
         code = compile(self.original_contents, self.path, 'exec')
 
         with _working_directory(self.source_dir):
