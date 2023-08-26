cc9e4490c104f2f9b11b273b7880c3562f7de020
Jonathan Guyer <guyer@nist.gov>
2023-08-26 00:34:54 -0400

    Strip workspace path

--- ntd2d/ntd2d_action/files/sphinxlog.py
+++ ntd2d/ntd2d_action/files/sphinxlog.py
@@ -30,7 +30,7 @@ class SphinxLog(File):
         prog_lineno = re.compile("(?P<FILE>.*)(?::(?P<LINE>[0-9]+))+?: (?P<TYPE>WARNING|ERROR): (?P<MESSAGE>.*)")
         prog_nolineno = re.compile("(?P<FILE>.*): (?P<TYPE>WARNING|ERROR): (?P<MESSAGE>.*)")
         
-        replacements = []
+        replacements = [["/github/workspace/", ""]]
         if hasattr(self.docs, "original_docs"):
             replacements.append([self.docs.docs_dir.as_posix(),
                                  self.docs.original_docs.docs_dir.as_posix()])
