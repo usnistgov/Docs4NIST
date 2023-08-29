f96be2a0c540c667961b4812ec6d44b50bceda61
Jonathan Guyer <guyer@nist.gov>
2023-08-29 09:42:25 -0400

    Use Napoleon numpy docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@ author = 'Jonathan E. Guyer'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosummary']
+              'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon',]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,6 +30,7 @@ sys.path.insert(0, (_action_root / "ntd2d").as_posix())
 
 autoclass_content = "both"
 
+napoleon_numpy_docstring = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
