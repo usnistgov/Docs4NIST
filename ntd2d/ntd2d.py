0d456676fdf473fe3400f639fbb916d0c7f8a84a
Jonathan Guyer <guyer@nist.gov>
2023-08-28 22:58:45 -0400

    Stop gathering return codes

--- ntd2d/ntd2d.py
+++ ntd2d/ntd2d.py
@@ -24,23 +24,21 @@ def main():
             gha_utils.debug(f"pre-build-command: {pre_build_command}", use_subprocess=True)
             subprocess.check_output(pre_build_command, shell=True, stderr=subprocess.STDOUT)
 
-    return_codes = []
-
     with gha_utils.group("Build HTML", use_subprocess=True):
         build_command = os.environ['INPUT_BUILD-HTML-COMMAND']
-        return_codes.append(docs.build_docs(build_command=build_command))
+        docs.build_docs(build_command=build_command)
 
     formats = os.environ['INPUT_FORMATS'].lower().split()
 
     if "pdf" in formats:
         with gha_utils.group("Build PDF", use_subprocess=True):
             build_command = os.environ['INPUT_BUILD-PDF-COMMAND']
-            return_codes.append(docs.build_docs(build_command=build_command))
+            docs.build_docs(build_command=build_command)
 
     if "epub" in formats:
         with gha_utils.group("Build ePub", use_subprocess=True):
             build_command = os.environ['INPUT_BUILD-EPUB-COMMAND']
-            return_codes.append(docs.build_docs(build_command=build_command))
+            docs.build_docs(build_command=build_command)
 
     with gha_utils.group("Update Pages", use_subprocess=True):
         repo = Repository(server_url=os.environ['GITHUB_SERVER_URL'],
@@ -52,10 +50,6 @@ def main():
 
         repo.update_pages(branch=os.environ['SANITIZED_REF_NAME'],
                           sha=os.environ['GITHUB_SHA'])
-
-    for code in return_codes:
-        if code != 0:
-            raise RuntimeError("Sphinx failed")
 
 if __name__ == "__main__":
     try:
