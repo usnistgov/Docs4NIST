5ef3ae374cbfc86604f80147dd945560bd3b45ec
Jonathan Guyer <guyer@nist.gov>
2023-09-05 14:37:04 -0400

    Show stderr

--- ntd2d/entrypoint.py
+++ ntd2d/entrypoint.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
         gha_utils.echo(f"what is happening?", use_subprocess=True)
         gha_utils.echo(f"stdout: {e.stdout.decode('utf-8')[:1000]}", use_subprocess=True)
-#         gha_utils.echo(f"stderr: {e.stderr.decode('utf-8')[:1000]}", use_subprocess=True)
+        gha_utils.echo(f"stderr: {e.stderr.decode('utf-8')[:1000]}", use_subprocess=True)
         sys.exit(1)
     except Exception as e:
         gha_utils.error("".join(traceback.format_exception(e)), use_subprocess=True)
