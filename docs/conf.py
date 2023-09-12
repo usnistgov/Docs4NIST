e23614f126303d1a7cd764e863b8b1e3a7961058
Jonathan Guyer <guyer@nist.gov>
2023-09-12 12:48:56 -0400

    Try piccolo_theme

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ napoleon_numpy_docstring = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
 
 html_show_copyright = False
