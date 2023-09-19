270e377dc19ca14d969464586965944bfcb69052
Jonathan Guyer <guyer@nist.gov>
2023-09-19 18:21:04 -0400

    Fix typo

--- ntd2d/ntd2d.py
+++ ntd2d/ntd2d.py
@@ -23,14 +23,14 @@ def main():
             gha_utils.debug(f"pre-build-command: {pre_build_command}", use_subprocess=True)
             subprocess.run(pre_build_command, check=True)
 
-    with gha_utils.group("Before Build HTML", use_subrocess=True):
+    with gha_utils.group("Before Build HTML", use_subprocess=True):
         subprocess.run(["ls", "_build/html"], check=True, buffer=1, text=True)
 
     with gha_utils.group("Build HTML", use_subprocess=True):
         build_command = os.environ['INPUT_BUILD-HTML-COMMAND']
         docs.build_docs(build_command=build_command)
 
-    with gha_utils.group("After Build HTML", use_subrocess=True):
+    with gha_utils.group("After Build HTML", use_subprocess=True):
         subprocess.run(["ls", "_build/html"], check=True, buffer=1, text=True)
 
     formats = os.environ['INPUT_FORMATS'].lower().split()
