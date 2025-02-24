1f7db55d8604bbe1916e1579d80ae2225ca7b32c
Jonathan Guyer <guyer@nist.gov>
2025-02-24 15:30:40 -0500

    Test conda environment

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ napoleon_numpy_docstring = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata-sphinx-theme'
 html_static_path = ['_static']
 
 html_show_copyright = False
